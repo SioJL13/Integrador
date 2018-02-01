f25b37a875d212adc5603274808edc5b1aeb9242
Siomara Jimenez <siomara@altomobile.io>
2018-02-01 11:30:33 -0600

    Fix heroku

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var express = requie('express');
+var express = require('express')
 var app = express();
 
-// set port
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
-// routes
-app.get("/", function(res,req){
-    res.render("index");
+app.get('/', function(req, res) {
+	res.render('index');
 })
 
-app.listen(port, function(){
-    console.log("app running");
+app.listen(port, function() {
+	console.log('app running')
 })
